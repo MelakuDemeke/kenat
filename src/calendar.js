e6b5fa17022fd466ead148be50033ab5fd498fd1
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-23 11:59:54 +0300

    feat: import month names and clean up TODO comments in calendar.js

--- src/calendar.js
+++ src/calendar.js
@@ -1,4 +1,4 @@
-// src/calendar.js
+import { monthNames } from './monthNames.js';
 
 /**
  * Ethiopian Calendar Library - Kenat
@@ -6,15 +6,6 @@
  * This file will implement core conversion logic and utilities
  * for working with the Ethiopian calendar and time system.
  */
-
-// TODO:
-// - [ ] Convert Gregorian to Ethiopian
-// - [ ] Convert Ethiopian to Gregorian
-// - [ ] Handle leap years in both calendars
-// - [ ] Support Ethiopian time (12-hour offset from Western time)
-// - [ ] Add now() to return current Ethiopian date and time
-// - [ ] Add formatting helpers (e.g., formatEthiopianDate())
-// - [ ] Validate and sanitize input
 
 /**
  * Convert Ethiopian date to Gregorian date.
