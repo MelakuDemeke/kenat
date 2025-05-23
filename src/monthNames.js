e85539f0037217d458d91617955001d17de53260
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-23 11:57:09 +0300

    feat: add month names for English and Amharic languages

---
+++ src/monthNames.js
@@ -0,0 +1,11 @@
+export const monthNames = {
+  en: [
+    "Meskerem", "Tikimt", "Hidar", "Tahsas", "Tir", "Yekatit",
+    "Megabit", "Miazia", "Ginbot", "Sene", "Hamle", "Nehase", "Pagume"
+  ],
+  am: [
+    "መስከረም", "ጥቅምት", "ህዳር", "ታህሳስ", "ጥር", "የካቲት",
+    "መጋቢት", "ሚያዝያ", "ግንቦት", "ሰኔ", "ሀምሌ", "ነሐሴ", "ጳጉሜ"
+  ],
+//   TODO: add oromifa, tigrigna, afar, somali, sidama, wolaytta, gambela languges
+};
