72b51e85cbe1b6807d9f74d1fd9bbeed3460790e
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-31 18:14:12 +0300

    test: add unit tests for monthDayFromDayOfYear function

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,5 @@
 import { dayOfYear } from "../src/utils";
+import { monthDayFromDayOfYear } from "../src/utils";
 
 describe('dayOfYear', () => {
     it('returns 1 for January 1st of a non-leap year', () => {
@@ -34,3 +35,40 @@ describe('dayOfYear', () => {
     });
 });
 
+describe('monthDayFromDayOfYear', () => {
+    it('returns January 1 for day 1 of a non-leap year', () => {
+        expect(monthDayFromDayOfYear(2023, 1)).toEqual({ month: 1, day: 1 });
+    });
+
+    it('returns January 31 for day 31 of a non-leap year', () => {
+        expect(monthDayFromDayOfYear(2023, 31)).toEqual({ month: 1, day: 31 });
+    });
+
+    it('returns February 1 for day 32 of a non-leap year', () => {
+        expect(monthDayFromDayOfYear(2023, 32)).toEqual({ month: 2, day: 1 });
+    });
+
+    it('returns February 28 for day 59 of a non-leap year', () => {
+        expect(monthDayFromDayOfYear(2023, 59)).toEqual({ month: 2, day: 28 });
+    });
+
+    it('returns March 1 for day 60 of a non-leap year', () => {
+        expect(monthDayFromDayOfYear(2023, 60)).toEqual({ month: 3, day: 1 });
+    });
+
+    it('returns February 29 for day 60 of a leap year', () => {
+        expect(monthDayFromDayOfYear(2024, 60)).toEqual({ month: 2, day: 29 });
+    });
+
+    it('returns March 1 for day 61 of a leap year', () => {
+        expect(monthDayFromDayOfYear(2024, 61)).toEqual({ month: 3, day: 1 });
+    });
+
+    it('returns December 31 for day 365 of a non-leap year', () => {
+        expect(monthDayFromDayOfYear(2023, 365)).toEqual({ month: 12, day: 31 });
+    });
+
+    it('returns December 31 for day 366 of a leap year', () => {
+        expect(monthDayFromDayOfYear(2024, 366)).toEqual({ month: 12, day: 31 });
+    });
+});
